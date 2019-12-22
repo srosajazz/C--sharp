94ebaf6cf22ee7213d82d4c067f1c545bef11f5c
Sergio Rosa <scarlosjazz@gmail.com>
2019-12-22 15:46:48 -0500

    Basic C # Data Types

---
+++ Course/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Course
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            SByte x = 100;
+            byte n1 = 126;
+            int n2 = 1000;
+            int n3 = 2147483647;
+            long n4 = 2147483648L;
+
+            Console.WriteLine(x);
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n4);
+        }
+    }
+}
