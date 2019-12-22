6edbcd3f0c5209f979ff5261d18da1d27e638363
Sergio Rosa <scarlosjazz@gmail.com>
2019-12-22 16:05:05 -0500

    Basic C # Data Types

--- Course/Program.cs
+++ Course/Program.cs
@@ -6,17 +6,47 @@ namespace Course
     {
         public static void Main(string[] args)
         {
-            SByte x = 100;
-            byte n1 = 126;
-            int n2 = 1000;
-            int n3 = 2147483647;
-            long n4 = 2147483648L;
 
-            Console.WriteLine(x);
+            int n1 = int.MinValue;
+            int n2 = int.MaxValue;
+            sbyte n3 = sbyte.MinValue;
+            decimal n4 = decimal.MaxValue;
+            
+
+
             Console.WriteLine(n1);
             Console.WriteLine(n2);
             Console.WriteLine(n3);
             Console.WriteLine(n4);
+
+            //bool complete = false;
+            //char genre = 'F';
+            //char word = '\u0041';
+            //SByte x = 100;
+            //byte n1 = 126;
+            //int n2 = 1000;
+            //int n3 = 2147483647;
+            //long n4 = 2147483648L;
+            //float n5 = 4.5f;
+            //double n6 = 4.5;
+            //string name = "Sergio Rosa";
+            //object obj1 = "Alex Jones";
+            //object obj2 = "Maia Ala";
+
+
+            //Console.WriteLine(word);
+            //Console.WriteLine(genre);
+            //Console.WriteLine(complete);
+            //Console.WriteLine(x);
+            //Console.WriteLine(n1);
+            //Console.WriteLine(n2);
+            //Console.WriteLine(n3);
+            //Console.WriteLine(n4);
+            //Console.WriteLine(n5);
+            //Console.WriteLine(n6);
+            //Console.WriteLine(name);
+            //Console.WriteLine(obj1);
+            //Console.WriteLine(obj2);
         }
     }
 }
